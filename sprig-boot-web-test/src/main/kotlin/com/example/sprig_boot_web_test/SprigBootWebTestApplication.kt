563824bba4a22b8071561dfcebd9a8b0f4d43937
s-abramov <sabramov96@gmail.com>
2024-06-04 23:06:47 +0300

    Thats one step for a new user, one giant leap for human laziness

---
+++ sprig-boot-web-test/src/main/kotlin/com/example/sprig_boot_web_test/SprigBootWebTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sprig_boot_web_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprigBootWebTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SprigBootWebTestApplication>(*args)
+}
